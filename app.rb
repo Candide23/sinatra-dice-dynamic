a940f894d7e3069e4d2cc630d69292fd54b697ca
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2024-05-08 16:33:36 +0000

    added Multiple parameters

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ get("/") do
   erb(:homepage)
 end
 
+get '/dice/85/8' do
+
+  "Hello World"
+
+end
+
 get("/dice/2/6") do
   @rolls = []
 
@@ -54,13 +60,15 @@ get("/dice/5/4") do
 end
 
 
-get("/dynamic/:number_of_dice/6") do
+get("/dynamic/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
+  @sides = params.fetch("how_many_sides").to_i
+
 
   @rolls = []
 
   @num_dice.times do
-    die = rand(1..6)
+    die = rand(1..@sides)
 
     @rolls.push(die)
   end
